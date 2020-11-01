b29a38e66f97ef93e7350e4d41da08e2ee515104
Parksohui <psh990525@naver.com>
2020-11-01 22:31:30 +0900

    회원가입 div 전환

--- Script.js
+++ Script.js
@@ -1,8 +1,14 @@
 // Main.html
 document.getElementById("login").addEventListener("click",login);
+document.getElementById("signin").addEventListener("click",join);
 
 function login(){
   $("#loginPage").css("display","none");
   $("#Choice").css("display","block");
   $("body").css("background-color","#f3f3f6");
 }
+
+ function join(){
+  $("#loginPage").css("display","none");
+  $("#Sign").css("display","block");
+ }
