719240657abf2068f4e211fdf9a8ff6f1b20a79c
201802085kimhyorin <gyfls7748@gmail.com>
2020-11-09 00:01:48 +0900

    json file name 수정

--- classupload.php
+++ classupload.php
@@ -4,7 +4,7 @@ $id=$_SESSION["id"];
 $jsonData = $_POST["sendFile"];
 $fileName = $_POST["fileName"];//Class
 
-$fileName="data/".$id."_".$fileName."json";//파일path 생성 data/id_Class.json
+$fileName="data/".$id."_".$fileName.".json";//파일path 생성 data/id_Class.json
 $fp = fopen($fileName, "w+");
 
 fwrite($fp, $jsonData);
