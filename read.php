a6844b95bf7729848705c75c7855332ade81794c
kimsoyeong <iloveit356@gmail.com>
2020-12-05 22:45:51 +0900

    Update read.php
    + 버그 수정

--- read.php
+++ read.php
@@ -1,26 +1,40 @@
 <?php
-$className = test_input($_POST['className']);
-$fileName = "./chatRooms.json";
+$fileName = "./data/Class/ClassID.json";
 
 $file = array();
 
 if(file_exists($fileName)){
-  $jsonfile = @file($fileName);
-  $arry = array();
-
-  for($i=0; $i<count($jsonfile); $i++){
-    $jsonfile[$i] = test_input($jsonfile[$i]);
-    array_push($arry, $jsonfile[$i]);
-  }
-  array_push($file, $arry);
+    $classID = file($fileName);
+    $arry = array(); // class id가 저장되는 곳
+    for($i=0; $i<count($classID); $i++){
+        $classID[$i] = test_input($classID[$i]);
+        $tmp = getDetail($classID[$i]);
+        array_push($arry, $tmp);
+    }
+    array_push($file, $arry);
 }
 else{
-  $arry = array();
-  array_push($file, $arry);
+    $arry = array();
+    array_push($file, $arry);
 }
 
 $output = json_encode($file);
 echo $output;
+
+
+
+function getDetail($cid){
+    $fName = "data/Class/".$cid.".json";//class
+    
+    if(file_exists($fName)){
+        $fp = fopen($fName,'r');
+        $txt = "";
+        $txt = test_input(fgets($fp));
+        fclose($fp);
+    }
+    
+    return $txt;
+}
 
 
 function test_input($data) {
