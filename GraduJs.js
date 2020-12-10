baf3ee880ce1d09b919add675ada26cf1e2deba2
Parksohui <psh990525@naver.com>
2020-12-10 15:30:44 +0900

    체크 확인문 추가

--- GraduJs.js
+++ GraduJs.js
@@ -17,48 +17,65 @@ function change3(){
 }
 // 다음 화면으로 나타내게끔 하는 함수
 
+function check_infovalue(interest, gender, id_list, time_list) {
+  if (interest == undefined || gender == undefined || id_list.length == 0 || time_list.length == 0) {
+    alert("선택하지 않은 항목이 있습니다!");
+    return false;
+  }
+
+  return true;
+}
+
 function info_save() {
-    var interest_ = $('input:radio[name="chk_title"]:checked').val();
-    var gender_=$('input:radio[name="chk_gender"]:checked').val();
-    // 관심사, 성별 value 받음
-    var id_list=[]
-    $('input[name="chk_ID"]:checked').each(function(){
-      var test=$(this).val();
-      id_list.push(test);
-    });
-    var time_list=[]
-    $('input[name="chk_time"]:checked').each(function(){
-      var temp=$(this).val();
-      time_list.push(temp);
-    });
-// 학번들 과 시간대 배열을 받음 
+  var interest_ = $('input:radio[name="chk_title"]:checked').val();
+  var gender_=$('input:radio[name="chk_gender"]:checked').val();
+  // 관심사, 성별 value 받음
+  var id_list=[]
+  $('input[name="chk_ID"]:checked').each(function(){
+    var test=$(this).val();
+    id_list.push(test);
+  });
+  var time_list=[]
+  $('input[name="chk_time"]:checked').each(function(){
+    var temp=$(this).val();
+    time_list.push(temp);
+  });
+
+  var bool = check_infovalue(interest_, gender_, id_list, time_list);
+  if (bool) {
+    // 학번들 과 시간대 배열을 받음
     Obj = {
-        interest: interest_,
-        gender: gender_,
-        id:id_list,
-        time:time_list
+      interest: interest_,
+      gender: gender_,
+      id:id_list,
+      time:time_list
     };
 
     url = "Graduation"
     var sendFile = JSON.stringify(Obj);
 
     $.ajax({
-        success: function (result) {
-            console.log(sendFile);
-            $.ajax({
-                url: "upload.php",
-                type: 'POST',
-                data: {
-                    sendFile: sendFile,
-                    fileName: url,
-                }
-            });
-            // 선택한 정보 전달 graduation에서 선택한 정보임을 전달
-            alert("upload완료");
-        },
-        error: function (result) {
-            console.log(sendFile);
-            alert("FAILED");
-        }
+      success: function (result) {
+        console.log(sendFile);
+        $.ajax({
+          url: "upload.php",
+          type: 'POST',
+          data: {
+            sendFile: sendFile,
+            fileName: url,
+          }
+        });
+        // 선택한 정보 전달 graduation에서 선택한 정보임을 전달
+        alert("upload완료");
+      },
+      error: function (result) {
+        console.log(sendFile);
+        alert("FAILED");
+      }
     });
+    return true;
+  }
+  else{
+    return false;
+  }
 }
--- Matching2.js
+++ Matching2.js
@@ -2,87 +2,89 @@ document.getElementById("Submit2").addEventListener("click", match);
 
 
 function replaceAll(str, org, dest) {
-    return str.split(org).join(dest);
+  return str.split(org).join(dest);
 }
 
 
 function match() {
 
-    // get user checks
-    var interest_ = $('input:radio[name="chk_title"]:checked').val();
-    var gender_ = $('input:radio[name="chk_gender"]:checked').val();
-    // 관심사, 성별 value 받음
-    var id_list = []
-    $('input[name="chk_ID"]:checked').each(function () {
-        var test = $(this).val();
-        id_list.push(test);
-    });
-    var time_list = []
-    $('input[name="chk_time"]:checked').each(function () {
-        var temp = $(this).val();
-        time_list.push(temp);
-    });
+  // get user checks
+  var interest_ = $('input:radio[name="chk_title"]:checked').val();
+  var gender_ = $('input:radio[name="chk_gender"]:checked').val();
+  // 관심사, 성별 value 받음
+  var id_list = []
+  $('input[name="chk_ID"]:checked').each(function () {
+    var test = $(this).val();
+    id_list.push(test);
+  });
+  var time_list = []
+  $('input[name="chk_time"]:checked').each(function () {
+    var temp = $(this).val();
+    time_list.push(temp);
+  });
 
-    // user info
-    let users = {
-        "interest": interest_,
-        "gender": gender_,
-        "id": id_list,
-        "time": time_list
-    };
+  // user info
+  let users = {
+    "interest": interest_,
+    "gender": gender_,
+    "id": id_list,
+    "time": time_list
+  };
 
 
-    let option_ = "G";
-    let reads = [];
-    $.ajax({
-        url: 'read.php',
-        type: 'POST',
-        data: {
-            option: option_,
-        },
-        dataType: "json", //받을 때만 적용
-        async: false,
-        success: function (response) {
-            reads = response[0];
-        },
-        error: function () {
-            alert("error");
-        }
-    });
-
-    let chatRooms = new Array();
-    let Roomids = new Array();
-    for (var i = 0; i < reads.length; i++) {
-        let str = reads[i];
-        let chat = str.split("|");
-        str = replaceAll(chat[1], "&quot;", '"');
-        var c = JSON.parse(str);
-        if (c['interest'] == users['interest']) {
-            chatRooms.push(c);
-            Roomids.push(chat[0]);
-        }
+  let option_ = "G";
+  let reads = [];
+  $.ajax({
+    url: 'read.php',
+    type: 'POST',
+    data: {
+      option: option_,
+    },
+    dataType: "json", //받을 때만 적용
+    async: false,
+    success: function (response) {
+      reads = response[0];
+    },
+    error: function () {
+      alert("error");
     }
+  });
 
-    cRScore = carScore(users, chatRooms);
-    console.log(cRScore);
+  let chatRooms = new Array();
+  let Roomids = new Array();
+  for (var i = 0; i < reads.length; i++) {
+    let str = reads[i];
+    let chat = str.split("|");
+    str = replaceAll(chat[1], "&quot;", '"');
+    var c = JSON.parse(str);
+    if (c['interest'] == users['interest']) {
+      chatRooms.push(c);
+      Roomids.push(chat[0]);
+    }
+  }
+
+  cRScore = carScore(users, chatRooms);
+  console.log(cRScore);
 
 
-    // sorting for matching
-    chatRooms, Roomids, cRScore = sortList(chatRooms, Roomids, cRScore); // 우선순위에 따라 정렬
+  // sorting for matching
+  chatRooms, Roomids, cRScore = sortList(chatRooms, Roomids, cRScore); // 우선순위에 따라 정렬
 
-    info_save();
+  let saveInfo=info_save();
 
+  if(saveInfo){
     let roomids = Roomids.join("|");
 
     $.ajax({
-        url: 'Chatting_.php',
-        type: 'POST',
-        traditional: true,
-        data: {
-            chatRoom: roomids,
-            classOrGrad: "Graduation",
-        },
+      url: 'Chatting_.php',
+      type: 'POST',
+      traditional: true,
+      data: {
+        chatRoom: roomids,
+        classOrGrad: "Graduation",
+      },
     });
+  }
 }
 
 
@@ -96,68 +98,68 @@ function swap(array,j){
 
 
 function sortList(chatRooms, Roomids, cRScore) { // 버블 소트 사용해봄
-    var n = chatRooms.length;
+  var n = chatRooms.length;
 
-    for (var i = n - 1; i > 0; i--) { // 0 ~ (i-1)
-        for (var j = 0; j < i; j++) { // 크기 순이 아니면 교환
-            if (cRScore[j]['prior'] < cRScore[j + 1]['prior']) { // piror점수에 따라 정렬
-              cRScore=swap(cRScore,j);
-              chatRooms=swap(chatRooms,j);
-              Roomids=swap(Roomids,j);
+  for (var i = n - 1; i > 0; i--) { // 0 ~ (i-1)
+    for (var j = 0; j < i; j++) { // 크기 순이 아니면 교환
+      if (cRScore[j]['prior'] < cRScore[j + 1]['prior']) { // piror점수에 따라 정렬
+        cRScore=swap(cRScore,j);
+        chatRooms=swap(chatRooms,j);
+        Roomids=swap(Roomids,j);
 
-            } else if (cRScore[j]['prior'] == cRScore[j + 1]['prior']) { // prior점수 같은 경우
-                if (cRScore[j]['noCount'] < cRScore[j + 1]['noCount']) { // 학번 점수 정렬
-                  cRScore=swap(cRScore,j);
-                  chatRooms=swap(chatRooms,j);
-                  Roomids=swap(Roomids,j);
+      } else if (cRScore[j]['prior'] == cRScore[j + 1]['prior']) { // prior점수 같은 경우
+        if (cRScore[j]['noCount'] < cRScore[j + 1]['noCount']) { // 학번 점수 정렬
+          cRScore=swap(cRScore,j);
+          chatRooms=swap(chatRooms,j);
+          Roomids=swap(Roomids,j);
 
-                } else if (cRScore[j]['noCount'] == cRScore[j + 1]['noCount']) { // 학번 점수 같은 경우
-                    if (cRScore[j]['timeCount'] < cRScore[j + 1]['timeCount']) { // 시간표 점수 정렬
-                      cRScore=swap(cRScore,j);
-                      chatRooms=swap(chatRooms,j);
-                      Roomids=swap(Roomids,j);
-                    }
-                }
-            }
-
+        } else if (cRScore[j]['noCount'] == cRScore[j + 1]['noCount']) { // 학번 점수 같은 경우
+          if (cRScore[j]['timeCount'] < cRScore[j + 1]['timeCount']) { // 시간표 점수 정렬
+            cRScore=swap(cRScore,j);
+            chatRooms=swap(chatRooms,j);
+            Roomids=swap(Roomids,j);
+          }
         }
-    }
+      }
 
-    return chatRooms, Roomids, cRScore;
+    }
+  }
+
+  return chatRooms, Roomids, cRScore;
 }
 
 function carScore(users, chatRooms) { // calculate priority score
-    let priority = []
-    for (var i = 0; i < chatRooms.length; i++) {
-        var prior = 0
-        let c = chatRooms[i];
-        var c2 = c['gender'];
-        var c3 = c['id'];
-        var c4 = c['time'];
+  let priority = []
+  for (var i = 0; i < chatRooms.length; i++) {
+    var prior = 0
+    let c = chatRooms[i];
+    var c2 = c['gender'];
+    var c3 = c['id'];
+    var c4 = c['time'];
 
-        if (c2 == users['gender']) prior += 1;
+    if (c2 == users['gender']) prior += 1;
 
-        let noCount = count(users['id'], c3); //studentid
+    let noCount = count(users['id'], c3); //studentid
 
-        let timeCount = count(users['time'], c4); // time available
+    let timeCount = count(users['time'], c4); // time available
 
-        priority.push({
-            prior,
-            noCount,
-            timeCount
-        });
-    }
+    priority.push({
+      prior,
+      noCount,
+      timeCount
+    });
+  }
 
-    return priority;
+  return priority;
 }
 
 function count(usersList, cN) {
-    let Count = 0;
-    for (var j = 0; j < cN.length; j++) {
-        if (usersList.includes(cN[j])) {
-            Count += 1
-        }
+  let Count = 0;
+  for (var j = 0; j < cN.length; j++) {
+    if (usersList.includes(cN[j])) {
+      Count += 1
     }
+  }
 
-    return Count;
+  return Count;
 }
